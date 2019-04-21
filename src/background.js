7160bb169d6f4c8f0e552d0e45af3cc67a2eea9b
helenmak <elenamak@ukr.net>
2019-04-21 22:08:04 +0300

    set necessary extension permissions

--- src/background.js
+++ src/background.js
@@ -1,26 +1,16 @@
 const apiv2 = 'https://translation.googleapis.com'
+const key = 'AIzaSyD9vAcdqFJ3PY2pKqsL_gqZgZfFHR7Ff68'
 
-chrome.runtime.onInstalled.addListener(function(data) {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log("The color is green.");
+/**
+ * Listen to events from content script and send api calls to google translation api
+ * Sends response back to content script
+ */
 
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
-  });
-});
-
-chrome.runtime.onMessage.addListener( // TODO: redo
+chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     switch(request.contentScriptQuery) {
       case "translateText":
-        const translateUrl = `${apiv2}/language/translate/v2?key=AIzaSyD9vAcdqFJ3PY2pKqsL_gqZgZfFHR7Ff68`
+        const translateUrl = `${apiv2}/language/translate/v2?key=${key}`
         const translateFetchOptions = {
           method: 'POST',
           body: JSON.stringify(request.options)
@@ -29,9 +19,10 @@ chrome.runtime.onMessage.addListener( // TODO: redo
           .then(response => response.json())
           .then(translated => sendResponse({ data: translated }))
           .catch(error => sendResponse({ error }))
-        return true;  // Will respond asynchronously.
+        return true;
+        
       case "getLanguages":
-        let languagesUrl = `${apiv2}/language/translate/v2/languages?key=AIzaSyD9vAcdqFJ3PY2pKqsL_gqZgZfFHR7Ff68`
+        let languagesUrl = `${apiv2}/language/translate/v2/languages?key=${key}`
         const target = request.options.target;
         if (target) languagesUrl = `${languagesUrl}&target=${target}`
         fetch(languagesUrl)
